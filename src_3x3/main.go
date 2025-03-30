f76dee142dc554eb4926ce607db7f61f80b5940e
Moth <moth.feedback@gmail.com>
2025-03-30 14:09:47 +0300

    init

---
+++ src_3x3/main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	// Определяем количество мин (1-2)
+	minesCount := rand.Intn(2) + 1
+	// Создаем поле
+	field := fillSquare(minesCount)
+	var userPosition int
+	var openPoints []int
+	var availablePoints = [9]int{11, 12, 13, 21, 22, 23, 31, 32, 33}
+	userMsg := ""
+	for {
+		clearConsole()
+		if userMsg != "" {
+			fmt.Println(userMsg)
+			userMsg = ""
+		}
+		printField(field)
+		fmt.Println("Enter position in format XY, example 11/23/31 etc.")
+		_, err := fmt.Fscan(os.Stdin, &userPosition)
+		if err != nil {
+			return
+		}
+
+		warning := validate(availablePoints, userPosition, openPoints)
+		if warning != "" {
+			userMsg = warning
+			continue
+		} else {
+			openPoints = append(openPoints, userPosition)
+		}
+
+		i, j := userPosition/10-1, userPosition%10-1
+		if field[i][j] == -3 {
+			clearConsole()
+			fmt.Println("BOOM!")
+			printOriginalField(field)
+			break
+		} else {
+			changeField(&field, i, j)
+		}
+
+		if len(openPoints) == len(availablePoints)-minesCount {
+			clearConsole()
+			fmt.Println("Congratulation!")
+			break
+		}
+	}
+
+}
+
+func fillSquare(minesCount int) [3][3]int {
+	field := [3][3]int{}
+	// Определяем их расположение внутри поля 3x3
+	// Если у нас есть поле NxN, где столбец i и строка j расположение первой мины,
+	// то у второй k, l, где k != i && l != j
+	x1, y1 := rand.Intn(3), rand.Intn(3)
+	var x2 int
+	var y2 int
+	if minesCount == 2 {
+		// Идём влево, чтобы при попадании в отрицательную область инвертировать её в положительную.
+		// Т.е. рассматриваем выход за левую границу, как "вход" на поле с правой границы
+		x2, y2 = x1-(rand.Intn(2)+1), y1-(rand.Intn(2)+1)
+		if x2 < 0 {
+			x2 += 3
+		}
+		if y2 < 0 {
+			y2 += 3
+		}
+	}
+	// Мина = -3
+	// Закрытая ячейка без мины = (0, 2)
+	// При открытии ячейки: 0 -> -10, 1 -> -1, 2 -> -2
+
+	// Заполнять будем построчно, каждый вложенный массив - 1 строка поля, начиная с нижнего
+	for i := range field {
+		for j := range field[i] {
+			if (i == x1 && j == y1) || (minesCount == 2 && (i == x2 && j == y2)) {
+				field[i][j] = -3
+			} else {
+				field[i][j] = 0 // Предварительно заполняем нулями, т.к. пока ещё не расставили мины
+			}
+		}
+	}
+
+	// Теперь для каждого элемента находим количество мин в его периметре
+	wg := sync.WaitGroup{}
+	for i := range field {
+		for j := range field[i] {
+			if field[i][j] != -3 {
+				wg.Add(1)
+				go searchMines(&field, i, j, &wg)
+			}
+		}
+	}
+	wg.Wait()
+
+	return field
+}
+
+func searchMines(field *[3][3]int, i int, j int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for k := -1; k < 2; k++ {
+		for l := -1; l < 2; l++ {
+			searchX := i + k
+			searchY := j + l
+			if searchX < 0 || searchX > 2 || searchY < 0 || searchY > 2 {
+				continue
+			} else if field[searchX][searchY] == -3 {
+				field[i][j] += 1
+			}
+		}
+	}
+}
+
+// Console clearing function
+func clearConsole() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	err := c.Run()
+	if err != nil {
+		return
+	}
+}
+
+func printField(field [3][3]int) {
+	var val string
+	for i := range len(field) {
+		for j := range field[i] {
+			if field[i][j] < 0 && field[i][j] != -3 {
+				if field[i][j] == -10 {
+					field[i][j] = 0
+				} else {
+					field[i][j] = -field[i][j]
+				}
+				val = strconv.Itoa(field[i][j]) // Преобразуем int к string
+			} else {
+				val = "?"
+			}
+			fmt.Print(val, " ")
+		}
+		fmt.Println()
+	}
+}
+
+func validate(availablePoints [9]int, userPosition int, openPoints []int) string {
+	for _, val := range openPoints {
+		if userPosition == val {
+			return "This point opened"
+		}
+	}
+	for _, val := range availablePoints {
+		if userPosition == val {
+			return ""
+		}
+	}
+	return "Not available point"
+}
+
+func changeField(field *[3][3]int, i int, j int) {
+	if field[i][j] == 0 {
+		field[i][j] = -10
+	} else if field[i][j] > 0 {
+		field[i][j] = -field[i][j]
+	}
+}
+
+func printOriginalField(field [3][3]int) {
+	for i := range field {
+		for j := range field[i] {
+			if field[i][j] == -3 {
+				fmt.Print("*", " ")
+			} else {
+				fmt.Print("?", " ")
+			}
+		}
+		fmt.Println()
+	}
+}
